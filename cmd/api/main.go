0c3b6730b0865d993133f5e9bb1946e1044546b5
Matheus Andrade <matheusandrade.ma2003@gmail.com>
2024-12-30 19:48:35 -0300

    feat api

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+	"github.com/matheusandrade23/go-bid/internal/api"
+	"github.com/matheusandrade23/go-bid/internal/services"
+)
 
 func main() {
-	fmt.Printf("jkghj")
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+
+	ctx := context.Background()
+	pool, err := pgxpool.New(ctx, fmt.Sprintf(
+    "user=%s password=%s host=%s port=%s dbname=%s",
+    os.Getenv("DATABASE_USER"),
+    os.Getenv("DATABASE_PASSWORD"),
+		os.Getenv("DATABASE_HOST"),
+		os.Getenv("DATABASE_PORT"),
+    os.Getenv("DATABASE_NAME"),
+	))
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer pool.Close()
+
+	if err := pool.Ping(ctx); err != nil {
+		panic(err)
+	}
+
+	api := api.Api{
+		Router: chi.NewMux(),
+		UserService: services.NewUserService(pool),
+	}
+	
+	api.BindRoutes()
+
+	fmt.Println("Starting Server on port:3080")
+	if err := http.ListenAndServe("localhost:3080", api.Router); err != nil {
+		panic(err)
+	}
 }
--- internal/api/api.go
+++ internal/api/api.go
@@ -2,9 +2,11 @@ package api
 
 import (
 	"github.com/go-chi/chi/v5"
+	"github.com/matheusandrade23/go-bid/internal/services"
 )
 
 type Api struct{
 	Router *chi.Mux
+	UserService services.UserService
 }
 
